9432aacd05aa3a288384733b2ccadf5a3dc1f1ef
crushyz007 <gracetyn2002@gmail.com>
2022-11-12 02:14:43 +0700

    add new line

--- src/components/feedbackBox.jsx
+++ src/components/feedbackBox.jsx
@@ -8,11 +8,17 @@ const FeedbackBox =() =>{
             <div className="feedbackBox-grid feedbackBox-font">
                 <div>รูป</div>
                 <div>Name
-                <div className="feedbackBox-commentPos">Rating</div>
+                {/* <div className="feedbackBox-commentPos">RatingRatingRatingRatingRatingRatingRatingRatingRatingRatingRatingRatingRating</div> */}
                 </div>
                 <div>Rate</div>
+                
+            </div>
+            <div className="grace">
+            gracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegrgracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegraceacegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegrace
+                {/* <div className="gracefont">gracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegracegrace</div> */}
             </div>
         </div>
+
     </div>
     )
 }
